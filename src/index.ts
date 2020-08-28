962808f1896d65e787fe3a2886e9d6500f82282a
wangpei <wangpei@huayun.com>
2020-08-28 11:50:05 +0800

    fix: package.json添加代码覆盖率

---
+++ src/index.ts
@@ -0,0 +1,5 @@
+export * from './components/Button';
+export { default as Button } from './components/Button';
+export * from './components/shared/global';
+export * from './components/shared/style';
+export * from './components/shared/animation';
