20ee8100be63b5a6d757a84fa0c250ca57448489
wangdson <wang1515123@163.com>
2020-09-17 09:23:56 +0000

    Deploying to gh-pages from  @ dde1ff84a295f032a2c9fa310321c59aa7c3496e 🚀

--- service-worker.js
+++ service-worker.js
@@ -14,7 +14,7 @@
 importScripts("https://storage.googleapis.com/workbox-cdn/releases/4.3.1/workbox-sw.js");
 
 importScripts(
-  "precache-manifest.f3bcedff4213112123d291b054274e27.js"
+  "precache-manifest.93e1cebf9e2439572a4b5f0cc93c59d4.js"
 );
 
 self.addEventListener('message', (event) => {
